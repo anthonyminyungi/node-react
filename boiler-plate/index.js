1e1545aac606ae6d663db49a6d88501443a18f25
anthonyminyungi <dbstnsdl12@naver.com>
2020-04-19 19:22:13 +0900

    #13 Added auth feature, and #14 logout feature

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -3,8 +3,8 @@ const app = express();
 const port = 5000;
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-
 const config = require('./config/key');
+const { auth } = require('./middleware/auth');
 const { User } = require('./models/User');
 
 // application/x-www-form-urlencoded
@@ -27,7 +27,7 @@ mongoose
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
   // 회원 가입 시 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
   const user = new User(req.body);
 
@@ -39,7 +39,7 @@ app.post('/register', (req, res) => {
   });
 });
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
   // 요청된 이메일을 데이터베이스에 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
@@ -65,10 +65,34 @@ app.post('/login', (req, res) => {
         // 토큰을 저장한다. 쿠키 / 로컬스토리지 등 / 세션스토리지 ...
         console.log(user.token);
         res
-          .cookie('x_auth', user.token)
+          .cookie('x_auth', user.token) // 여기서는 쿠키
           .status(200)
           .json({ loginSuccess: true, userId: user._id });
       });
+    });
+  });
+});
+
+app.get('/api/users/auth', auth, (req, res) => {
+  // 여기까지 미들웨어를 통과해왔다면 Authentication이 True라는 말.
+
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
+
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: '' }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
     });
   });
 });
